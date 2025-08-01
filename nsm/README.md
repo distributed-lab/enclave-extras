1c4e4edd2aa8c0a52b771a549b883b6dc13ddd51
Zaptoss <zaptosgg@gmail.com>
2025-08-01 16:45:41 +0300

    Remove Rust/C dependencies in nsm package

--- nsm/README.md
+++ nsm/README.md
@@ -1,23 +1,4 @@
 # Go NSM API
-Wrapper for Nitro Secure Module API for Golang. To compile a project with this library, use pkg-config. 
-
-libnsm.pc allows you to specify the path to the libnsm library, so the library can be placed anywhere, and the path to lib must be specified in this file. See the examples.
-
-
-### libnsm.pc
-```pkg-cofnig
-libdir=/path/to/dir/with/lib
-
-Name: libnsm
-Description: Nitro Secure Module API
-Version: v0.4.0
-Libs: -L${libdir} -lnsm
-```
-
-### Specify path to directory with .pc files
-```
-export PKG_CONFIG_PATH=/path/to/libnsm.pc:$PKG_CONFIG_PATH
-```
-
+Adapter for [hf/nsm](https://github.com/hf/nsm)
 <hr>
 TODO: Describe functions and add examples
--- nsm/main.go
+++ nsm/main.go
@@ -1,6 +1,13 @@
 package nsm
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/hf/nsm"
+	"github.com/hf/nsm/request"
+	"github.com/hf/nsm/response"
+)
 
 var (
 	ErrNSMDeviceAbsent  = fmt.Errorf("nsm: device is absent")
@@ -9,6 +16,15 @@ var (
 	ErrNonceTooLarge    = fmt.Errorf("nsm: nonce exceeds maximum size")
 	ErrPubKeyTooLarge   = fmt.Errorf("nsm: public key exceeds maximum size")
 	ErrBufferTooLarge   = fmt.Errorf("nsm: buffer exceeds maximum size")
+)
+
+const (
+	// https://github.com/aws/aws-nitro-enclaves-sdk-c/blob/74a35c70b7b0b855cbb9773178a5c8c5ec405363/source/attestation.c#L20
+	NSM_MAX_ATTESTATION_DOC_SIZE = 16 * 1024
+	// https://docs.aws.amazon.com/enclaves/latest/user/verify-root.html
+	NSM_MAX_PCR_SIZE           = 64
+	NSM_MAX_USER_DATA_SIZE     = 1024
+	NSM_MAX_RANDOM_BUFFER_SIZE = 256
 )
 
 // Return PCRx condition to be used when creating a KMS key
@@ -20,57 +36,79 @@ func PCRxCondition(pcrIndex int) string {
 //
 // PCRx = SHA384(PCRx || data)
 func ExtendPCR(pcrIndex int, data []byte) (newPCRValue []byte, err error) {
-	nsmFd := nsmLibInit()
-	if nsmFd == -1 {
-		return nil, ErrNSMDeviceAbsent
-	}
-
 	if !validatePCRIndex(pcrIndex) {
 		return nil, ErrInvalidPCRIndex
 	}
 
-	extendedPCR, errorCode := nsmExtendPCR(nsmFd, uint16(pcrIndex), data)
-	if errorCode != Success {
-		return nil, fmt.Errorf("nsm: error code: %d", errorCode)
+	sess, err := nsm.OpenDefaultSession()
+	if err != nil {
+		return nil, fmt.Errorf("failed to open default session: %w", err)
+	}
+	defer sess.Close()
+
+	res, err := sess.Send(&request.ExtendPCR{
+		Index: uint16(pcrIndex),
+		Data:  data,
+	})
+	if err != nil {
+		return nil, err
 	}
 
-	return extendedPCR, nil
+	if res.Error != "" {
+		return nil, errors.New(string(res.Error))
+	}
+
+	if res.ExtendPCR == nil {
+		return nil, errors.New("NSM device did not return an pcr description")
+	}
+
+	return res.ExtendPCR.Data, nil
 }
 
 func DescribePCR(pcrIndex int) (isLocked bool, data []byte, err error) {
-	nsmFd := nsmLibInit()
-	if nsmFd == -1 {
-		return false, nil, ErrNSMDeviceAbsent
+	sess, err := nsm.OpenDefaultSession()
+	if err != nil {
+		return false, nil, fmt.Errorf("failed to open default session: %w", err)
+	}
+	defer sess.Close()
+
+	res, err := sess.Send(&request.DescribePCR{
+		Index: uint16(pcrIndex),
+	})
+	if err != nil {
+		return false, nil, err
 	}
 
-	if !validatePCRIndex(pcrIndex) {
-		return false, nil, ErrInvalidPCRIndex
+	if res.Error != "" {
+		return false, nil, errors.New(string(res.Error))
 	}
 
-	locked, pcrData, errorCode := nsmDescribePCR(nsmFd, uint16(pcrIndex))
-	if errorCode != Success {
-		return false, nil, fmt.Errorf("nsm: error code: %d", errorCode)
+	if res.DescribePCR == nil {
+		return false, nil, errors.New("NSM device did not return an pcr description")
 	}
 
-	return locked, pcrData, nil
+	return res.DescribePCR.Lock, res.DescribePCR.Data, nil
 }
 
 // Lock PCRx. Locked PCRs cannot be extended
 //
 // PCRs 0-15 locked by default
 func LockPCR(pcrIndex int) error {
-	nsmFd := nsmLibInit()
-	if nsmFd == -1 {
-		return ErrNSMDeviceAbsent
+	sess, err := nsm.OpenDefaultSession()
+	if err != nil {
+		return fmt.Errorf("failed to open default session: %w", err)
+	}
+	defer sess.Close()
+
+	res, err := sess.Send(&request.LockPCR{
+		Index: uint16(pcrIndex),
+	})
+	if err != nil {
+		return err
 	}
 
-	if !validatePCRIndex(pcrIndex) {
-		return ErrInvalidPCRIndex
-	}
-
-	errorCode := nsmLockPCR(nsmFd, uint16(pcrIndex))
-	if errorCode != Success {
-		return fmt.Errorf("nsm: error code: %d", errorCode)
+	if res.Error != "" {
+		return errors.New(string(res.Error))
 	}
 
 	return nil
@@ -78,43 +116,56 @@ func LockPCR(pcrIndex int) error {
 
 // Lock range of PCRs: from 0 to pcrsRange
 func LockPCRs(pcrsRange int) error {
-	nsmFd := nsmLibInit()
-	if nsmFd == -1 {
-		return ErrNSMDeviceAbsent
+	sess, err := nsm.OpenDefaultSession()
+	if err != nil {
+		return fmt.Errorf("failed to open default session: %w", err)
+	}
+	defer sess.Close()
+
+	res, err := sess.Send(&request.LockPCRs{
+		Range: uint16(pcrsRange),
+	})
+	if err != nil {
+		return err
 	}
 
-	if !validatePCRIndex(pcrsRange) {
-		return ErrInvalidPCRIndex
-	}
-
-	errorCode := nsmLockPCR(nsmFd, uint16(pcrsRange))
-	if errorCode != Success {
-		return fmt.Errorf("nsm: error code: %d", errorCode)
+	if res.Error != "" {
+		return errors.New(string(res.Error))
 	}
 
 	return nil
 }
 
 // Get Nitro Secure Module configuration
-func GetDescription() (*NSMDescription, error) {
-	nsmFd := nsmLibInit()
-	if nsmFd == -1 {
-		return nil, ErrNSMDeviceAbsent
+func GetDescription() (*response.DescribeNSM, error) {
+	sess, err := nsm.OpenDefaultSession()
+	if err != nil {
+		return nil, fmt.Errorf("failed to open default session: %w", err)
+	}
+	defer sess.Close()
+
+	res, err := sess.Send(&request.DescribeNSM{})
+	if err != nil {
+		return nil, err
 	}
 
-	nsmDescription, errorCode := nsmGetDescription(nsmFd)
-	if errorCode != Success {
-		return nil, fmt.Errorf("nsm: error code: %d", errorCode)
+	if res.Error != "" {
+		return nil, errors.New(string(res.Error))
 	}
 
-	return &nsmDescription, nil
+	if res.DescribeNSM == nil {
+		return nil, errors.New("NSM device did not return an nsm description")
+	}
+
+	return res.DescribeNSM, nil
 }
 
 func GetAttestationDoc(userData []byte, nonce []byte, pubKey []byte) ([]byte, error) {
-	nsmFd := nsmLibInit()
-	if nsmFd == -1 {
-		return nil, ErrNSMDeviceAbsent
+	sess, err := nsm.OpenDefaultSession()
+	if err != nil {
+		return nil, fmt.Errorf("failed to open default session: %w", err)
 	}
+	defer sess.Close()
 
 	if len(userData) > NSM_MAX_USER_DATA_SIZE {
 		return nil, ErrUserDataTooLarge
@@ -128,28 +179,36 @@ func GetAttestationDoc(userData []byte, nonce []byte, pubKey []byte) ([]byte, er
 		return nil, ErrPubKeyTooLarge
 	}
 
-	attDoc, errorCode := nsmGetAttestationDoc(nsmFd, userData, nonce, pubKey)
-	if errorCode != Success {
-		return nil, fmt.Errorf("nsm: error code: %d", errorCode)
+	res, err := sess.Send(&request.Attestation{
+		Nonce:     nonce,
+		UserData:  userData,
+		PublicKey: pubKey,
+	})
+	if err != nil {
+		return nil, err
 	}
 
-	return attDoc, nil
+	if res.Error != "" {
+		return nil, errors.New(string(res.Error))
+	}
+
+	if res.Attestation == nil || res.Attestation.Document == nil {
+		return nil, errors.New("NSM device did not return an attestation")
+	}
+
+	return res.Attestation.Document, nil
 }
 
 // Get random bytes from NSM up to 256 byte per call
 func GetRandom(buf []byte) error {
-	nsmFd := nsmLibInit()
-	if nsmFd == -1 {
-		return ErrNSMDeviceAbsent
+	sess, err := nsm.OpenDefaultSession()
+	if err != nil {
+		return fmt.Errorf("failed to open default session: %w", err)
 	}
+	defer sess.Close()
 
-	if len(buf) > NSM_MAX_RANDOM_BUFFER_SIZE {
-		return ErrBufferTooLarge
-	}
-
-	errorCode := nsmGetRandom(nsmFd, buf)
-	if errorCode != Success {
-		return fmt.Errorf("nsm: error code: %d", errorCode)
+	if _, err = sess.Read(buf); err != nil {
+		return fmt.Errorf("failed to read random data: %w", err)
 	}
 
 	return nil
